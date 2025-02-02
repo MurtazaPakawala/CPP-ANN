09175a1c36fc3920c8e4965a23e5aae82c5523f8
Murtaza Pakawala <murtazapakawala@student.unsw.edu.au>
2025-02-02 10:11:36 -0500

    basic outline of the class

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+// making the NET class 
+class Net {
+    Net();
+
+    void feedForward();
+    void backProp();
+    void getResult() const;
+
+};
+
+
+int main() {
+    // there has to be neural network (providing some sort of structure)
+    // which has layers
+    // layers have neuron
+
+    Net myNet = Net();
+    myNet.fe
+    return 0;
+}
